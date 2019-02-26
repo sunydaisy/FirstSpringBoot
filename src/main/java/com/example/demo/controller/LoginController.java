b6887bb44c923287734c1957f4871b553833b5ad
sunydaisy <sunydaisy@163.com>
2019-02-26 19:44:47 +0800

    111

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,18 +1,14 @@
 package com.example.demo.controller;
 
 import javax.validation.Valid;
-
-import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.service.LoginService;
 import com.example.demo.vo.UserInfoVO;
-
 import lombok.extern.slf4j.Slf4j;
 
 @RestController

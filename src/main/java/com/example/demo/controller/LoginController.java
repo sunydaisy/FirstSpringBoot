5fa5db07ad29def651c1e573b35891c26dba2768
sunydaisy <sunydaisy@163.com>
2019-03-14 18:09:52 +0800

    111

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,19 +1,19 @@
 package com.example.demo.controller;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.service.LoginService;
 import com.example.demo.vo.UserInfoVO;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/first")
+@Slf4j
 public class LoginController {
 	
 	@Autowired
@@ -21,6 +21,7 @@ public class LoginController {
 	
 	@PostMapping("/login")
 	public String login(@Valid @RequestBody UserInfoVO user,BindingResult bindingResult) {
+		log.info("新的请求参数为："+user.toString());
 		if(bindingResult.hasErrors()) {
 			return "请求参数错误----"+bindingResult.getFieldError().getDefaultMessage();
 		}
